7896bfdab3cf826daf17025fbf22291c91e3a080
om sai mishara <omsaimishra95568@gmail.com>
2026-01-12 11:07:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -68,7 +68,7 @@ const searchStudent = async () => {
   return (
     <div className="page">
       <h1>Student Management</h1>
-      <p className="subtitle">Demo Applicationschikus</p>
+      <p className="subtitle">Demo Application</p>
 
       <div className="grid">
         {/* ➕ Add Student */}
