380a6da61da8e548fcb28da2d6301e6bd4157ff6
om sai mishara <omsaimishra95568@gmail.com>
2026-02-25 16:45:13 +0530

    Update afterinstall.sh

--- scripts/afterinstall.sh
+++ scripts/afterinstall.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 set -e
 
+cd /opt/codedeploy-agent/deployment-root/*/*/deployment-archive
+
 AWS_REGION="ap-south-1"
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 
 echo "Logging into ECR..."
-
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-echo "Exporting variables for docker-compose..."
-
-export AWS_REGION=$AWS_REGION
-export ACCOUNT_ID=$ACCOUNT_ID
-
 echo "Pulling latest images..."
-
 docker compose pull
-
-echo "Starting containers..."
-
-docker compose up -d
